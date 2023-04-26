f28a53b6e366a2180bcdd3a74b8fc0640bc1c16d
Alan Ho <aho0593@gmail.com>
2023-04-26 05:39:27 +0000

    Created dummy anime entry to ul #1

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ for (let i = currentYear; i >= 2000; i--) {
   $yearDropdown.append(yearOption);
 }
 // Creates year dropdown option; from 2000 to current year.
+
+// const $animeList = document.querySelector('#anime-list');
+
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.jikan.moe/v4/seasons/now?page=1');
+// xhr.responseType = 'json';
+// xhr.addEventListener('load', function () {
+//   console.log(xhr.status);
+//   console.log(xhr.response);
+//   for (let i = 0; i < xhr.response.length; i++) {
+//     const liTag = document.createElement('li');
+//     liTag.textContent = xhr.response[i].name;
+//     $userList.appendChild(liTag);
+//   }
+// });
+// xhr.send();
