814517ac6036617c634a949439ee4b2dfe267770
Alan Ho <aho0593@gmail.com>
2023-04-26 22:03:50 +0000

    Click event listeners added to info, play and bookmark buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+/* exported $ulChild $playConfirm  $bookmarkConfirm */
 const $yearDropdown = document.querySelector('#year-select');
+const $ulList = document.querySelector('ul');
+const $ulChild = $ulList.childNodes;
 
 const currentYear = new Date().getFullYear();
 
@@ -8,7 +11,70 @@ for (let i = currentYear; i >= 2000; i--) {
   yearOption.value = i;
   $yearDropdown.append(yearOption);
 }
+
 // Creates year dropdown option; from 2000 to current year.
+
+let toggleInfoModal = false;
+const $infoModal = document.querySelector('#info-modal');
+const $infoExit = document.querySelector('.info-exit');
+
+let togglePlayModal = false;
+const $playModal = document.querySelector('#play-modal');
+const $playConfirm = document.querySelector('.play-confirm');
+const $playExit = document.querySelector('.play-exit');
+
+let toggleBookmarkModal = false;
+const $bookmarkModal = document.querySelector('#bookmark-modal');
+const $bookmarkConfirm = document.querySelector('.bookmark-confirm');
+const $bookmarkExit = document.querySelector('.bookmark-exit');
+
+$ulList.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'I') {
+    return;
+  }
+
+  const classList = event.target.classList;
+
+  for (let i = 0; i < classList.length; i++) {
+    if (classList[i] === 'info') {
+      toggleInfoModal = !toggleInfoModal;
+      if (toggleInfoModal === true) {
+        $infoModal.classList.remove('hidden');
+      }
+    } else if (classList[i] === 'play') {
+      togglePlayModal = !togglePlayModal;
+      if (togglePlayModal === true) {
+        $playModal.classList.remove('hidden');
+      }
+    } else if (classList[i] === 'bookmark') {
+      toggleBookmarkModal = !toggleBookmarkModal;
+      if (toggleBookmarkModal === true) {
+        $bookmarkModal.classList.remove('hidden');
+      }
+    }
+  }
+});
+
+$infoExit.addEventListener('click', function (event) {
+  toggleInfoModal = !toggleInfoModal;
+  if (toggleInfoModal === false) {
+    $infoModal.classList.add('hidden');
+  }
+});
+
+$playExit.addEventListener('click', function (event) {
+  togglePlayModal = !togglePlayModal;
+  if (togglePlayModal === false) {
+    $playModal.classList.add('hidden');
+  }
+});
+
+$bookmarkExit.addEventListener('click', function (event) {
+  toggleBookmarkModal = !toggleBookmarkModal;
+  if (toggleBookmarkModal === false) {
+    $bookmarkModal.classList.add('hidden');
+  }
+});
 
 // const $animeList = document.querySelector('#anime-list');
 
@@ -17,7 +83,7 @@ for (let i = currentYear; i >= 2000; i--) {
 // xhr.responseType = 'json';
 // xhr.addEventListener('load', function () {
 //   console.log(xhr.status);
-//   console.log(xhr.response);
+//   console.log(xhr.response.data[4].synopsis);
 //   for (let i = 0; i < xhr.response.length; i++) {
 //     const liTag = document.createElement('li');
 //     liTag.textContent = xhr.response[i].name;
