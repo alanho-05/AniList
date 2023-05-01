d7221670008a045c0a41e30ca4e8647243a2e43c
Alan Ho <aho0593@gmail.com>
2023-05-01 16:38:13 +0000

    Storing data to local storage before unload

--- js/data.js
+++ js/data.js
@@ -1,9 +1,20 @@
 /* exported data */
 
-const data = {
+let data = {
   view: 'anime-list',
   list: [],
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+const previousEntryJSON = localStorage.getItem('watchlist-local-storage');
+
+document.addEventListener('beforeunload', function (event) {
+  const entryJSON = JSON.stringify(data);
+  localStorage.setItem('watchlist-local-storage', entryJSON);
+});
+
+if (previousEntryJSON !== null) {
+  data = JSON.parse(previousEntryJSON);
+}
